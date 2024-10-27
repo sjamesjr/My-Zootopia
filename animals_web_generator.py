43a31171ada140e1c8bc71c9fe0ce5acd6cf031e
sjamesjr <sam@barprints.com>
2024-10-27 13:36:54 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,19 +12,25 @@ animals_data = load_data('animals_data.json')
 
 def print_animal_info(animals_data):
     """Iterates through the animals and prints Name, Diet, first location, and Type if they exist."""
+    output = ''  # define an empty string
     for animal in animals_data:
-        # Check if the 'name' field exists and print it
-        if 'name' in animal:
-            print(f"Name: {animal['name']}")
-            if 'characteristics' in animal:
-                characteristics = animal['characteristics']
-                if 'diet' in characteristics:
-                    print(f"Diet: {characteristics['diet']}")
-                if 'type' in characteristics:
-                    print(f"Type: {characteristics['type']}")
-            if 'locations' in animal and animal['locations']:
-                print(f"First Location: {animal['locations'][0]}")
-            print()  # Print a newline for better readability between entries
+        # append information to each string
+        output += '<li class="cards__item">'
+        output += f"Name: {animal['name']}<br/>\n"
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+        if 'type' in animal['characteristics']:
+            output += f"Type: {animal['characteristics']['type']}<br/>\n"
+        else:
+            output += "Type: N/A <br/>\n"  # Add a placeholder if 'type' is missing
+        output += f"Location: {animal['locations'][0]}<br/>\n"
+        output += '</li>'
+
+    return output
 
 
-print_animal_info(animals_data)
+with open("animals_template.html", "r") as file:
+    content = file.read()
+    animal_info = print_animal_info(animals_data)
+    new_content = content.replace("__REPLACE_ANIMALS_INFO__", animal_info)
+
+print(new_content)
